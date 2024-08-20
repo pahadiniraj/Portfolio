873b323f689ac9467cb76548729da1953b227c96
Niraj Sharma <sharma12345niraj.com>
2024-08-20 15:24:55 +0545

    Navbar added

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FloatingDockDemo } from "../UI/Components/FlotingDocComp";
+
+const Navbar = () => {
+  return (
+    <>
+      <FloatingDockDemo />
+    </>
+  );
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { PluginAPI } from "tailwindcss/types/config";
 
 const config: Config = {
   content: [
@@ -8,13 +9,65 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#1D4ED8", // Custom primary color
+        secondary: "#9333EA", // Custom secondary color
+        accent: "#F59E0B", // Custom accent color
+        muted: "#6B7280", // Custom muted color
+        success: "#10B981", // Custom success color
+        danger: "#EF4444", // Custom danger color
+        warning: "#FBBF24", // Custom warning color
+        info: "#3B82F6", // Custom info color
+      },
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"], // Default font for general text
+        heading: ["Poppins", "sans-serif"], // Font for headings
+        cursive: ["Dancing Script", "cursive"], // Cursive font for specific text
+      },
+      fontSize: {
+        xs: ".75rem", // 12px
+        sm: ".875rem", // 14px
+        base: "1rem", // 16px
+        lg: "1.125rem", // 18px
+        xl: "1.25rem", // 20px
+        "2xl": "1.5rem", // 24px
+        "3xl": "1.875rem", // 30px
+        "4xl": "2.25rem", // 36px
+        "5xl": "3rem", // 48px
+        "6xl": "4rem", // 64px
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "btn-gradient": "linear-gradient(to bottom left, #00C9FF, #92FE9D)",
+      },
+      backgroundClip: {
+        text: "text",
+      },
+      textFillColor: {
+        transparent: "transparent",
       },
     },
   },
-  plugins: [],
+  variants: {
+    extend: {
+      backgroundImage: ["responsive", "hover"],
+      backgroundClip: ["responsive", "hover"],
+      textFillColor: ["responsive", "hover"],
+    },
+  },
+  plugins: [
+    function ({ addUtilities }: PluginAPI) {
+      addUtilities({
+        ".text-gradient": {
+          background: "linear-gradient(to bottom left, #00C9FF, #92FE9D)",
+          "-webkit-background-clip": "text",
+          "-webkit-text-fill-color": "transparent",
+        },
+        ".text-glow": {
+          textShadow: "0 0 10px rgba(255, 255, 255, 0.7)", // Glow effect
+        },
+      });
+    },
+  ],
 };
+
 export default config;
