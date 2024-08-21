766539eeac00aab7b7ffb527825d1c983d9c4e87
pahadiniraj <sharma12345niraj@gmail.com>
2024-08-21 16:23:15 +0545

    changes

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-  return <div>Hello world i'm niraj</div>;
+  return <div>Hello world i'm niraj.I'm ok to be back again </div>;
 };
 
 export default About;
