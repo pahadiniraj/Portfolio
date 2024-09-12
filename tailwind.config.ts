aed49b041a6230708808bd3d81dd90ded0f28be9
pahadiniraj <sharma12345niraj@gmail.com>
2024-09-12 10:07:11 +0545

    reset taildwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,68 +1,17 @@
 import type { Config } from "tailwindcss";
-import { PluginAPI } from "tailwindcss/types/config";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      boxShadow: {
-        custom: "1px 0 8px white",
-        "inner-md": "inset 4px 2px 5px rgba(0, 0, 0, 0.5)",
-      },
-      colors: {
-        primary: "#1D4ED8",
-        secondary: "#9333EA",
-        accent: "#F59E0B",
-        muted: "#6B7280",
-        success: "#10B981",
-        danger: "#EF4444",
-        warning: "#FBBF24",
-        info: "#3B82F6",
-      },
-      fontFamily: {
-        sans: ["Open Sans", "sans-serif"],
-        heading: ["Poppins", "sans-serif"],
-        cursive: ["Dancing Script", "cursive"],
-      },
-      fontSize: {
-        xxs: ".65rem",
-        xs: ".75rem",
-        sm: ".875rem",
-        base: "1rem",
-        lg: "1.125rem",
-        xl: "1.25rem",
-        "2xl": "1.5rem",
-        "3xl": "1.875rem",
-        "4xl": "2.25rem",
-        "5xl": "3rem",
-        "6xl": "4rem",
-      },
-      backgroundImage: {
-        "btn-gradient": "linear-gradient(to bottom left, #614385,  #516395)",
-      },
-      backgroundClip: {
-        text: "text",
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    function ({ addUtilities }: PluginAPI) {
-      addUtilities({
-        ".text-glow": {
-          textShadow: "0 0 10px rgba(255, 255, 255, 0.7)",
-        },
-        ".scrollbar-hide": {
-          /* Hide scrollbar for Firefox, IE, Edge */
-          scrollbarWidth: "none",
-          /* Hide scrollbar for Chrome, Safari and Opera */
-        },
-      });
-    },
-  ],
+  plugins: [],
 };
-
 export default config;
