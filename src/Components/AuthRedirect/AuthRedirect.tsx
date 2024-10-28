1b1cd6f65971bc9bbdc9116e64dbdd85d9252244
pahadiniraj <sharma12345niraj@gmail.com>
2024-10-28 14:01:51 +0545

    middleware added

--- src/Components/AuthRedirect/AuthRedirect.tsx
+++ src/Components/AuthRedirect/AuthRedirect.tsx
@@ -1,20 +1,28 @@
 "use client";
 import { useGetUserQuery } from "@/Redux/Services/user";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import React, { useEffect } from "react";
 
 const AuthRedirect = () => {
   const { data, isLoading } = useGetUserQuery();
   const router = useRouter();
+  const pathname = usePathname();
+
+  const exemptRoutes = ["/about", "/contact", "/", "/portfolio", "/why-me"];
 
   useEffect(() => {
-    if (isLoading) return;
+    const isExempt =
+      exemptRoutes.includes(pathname) ||
+      pathname.startsWith("/user-profile/") ||
+      pathname.startsWith("/project/");
+
+    if (isLoading || isExempt) return;
     if (!data) {
       router.push("/");
       return;
     }
-    return router.push("/dashboard/setting");
-  }, [isLoading, data]);
+    router.push("/dashboard/setting");
+  }, [isLoading, data, pathname]);
 
   return <div></div>;
 };
